cddd882e6a16d48f643954417ee4509b7ade9319
zac-zaoxuanzhou <zaczzx@163.com>
2018-04-22 21:33:28 -0500

    install redux-promise, prevent formdefault

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,14 +1,34 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
 
   render () {
     return (
-      <form className="input-group">
-        <input></input>
-         <span className="input-group-btn">
-           <button type="submit" className="btn btn-secondary">Submit</button>
-         </span>
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a five day forecast in your favorite city"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
       </form>
     );
   }
